5b5dcac6a9216524e2c18328084aab86cc47b673
mariana-yanez <nitayanez17@gmail.com>
2021-08-22 14:06:17 -0300

    Entrega 1 js

--- js/login.js
+++ js/login.js
@@ -3,4 +3,27 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+    document.getElementById("submit").addEventListener("click", function() {
+
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletados = true;
+
+        if (inputEmail.value === ""){
+            camposCompletados = false;
+        }
+
+        if (inputPassword.value === ""){
+            camposCompletados = false;
+        }
+
+        if (camposCompletados){
+
+            window.location = "inicio.html";
+
+        }else {
+            alert("Debes ingresar los datos para continuar!")
+        }
+    })
+
 });
